446327243bb71bac8545333d81d0bcce85e62662
SwaroopKS_Appi0007 <106536825+SwaroopKadabaSriraj@users.noreply.github.com>
2023-10-01 23:35:35 +0530

    Create Max consecutive ones.cpp

---
+++ Day - 07/Max consecutive ones.cpp
@@ -0,0 +1,18 @@
+/*CodeStudio solution*/
+
+int consecutiveOnes(vector<int>& arr){
+    //Write your code here.
+    int total = 0;
+    int res = 0;
+        
+    for(auto n : arr) {
+        if(n == 1) {
+            total++;
+            res = max(res, total);
+        } 
+        else {
+            total = 0;
+        }
+    }
+    return res;
+}
