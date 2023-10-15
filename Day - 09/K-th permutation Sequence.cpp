b7afa7f139eb720ed46688fa2021254b684b4245
SwaroopKS_Appi0007 <106536825+SwaroopKadabaSriraj@users.noreply.github.com>
2023-10-15 23:59:32 +0530

    Create K-th permutation Sequence.cpp

---
+++ Day - 09/K-th permutation Sequence.cpp
@@ -0,0 +1,54 @@
+/*CodeStudio solution*/
+
+string kthPermutation(int n, int k) {
+    // Write your code here.
+    vector<int>arr;
+    string ans="";
+    int fact=1;
+    
+    for(int i=1;i<n;i++){
+        fact=fact*i;
+        arr.push_back(i);
+    }
+    arr.push_back(n);
+    k=k-1;
+    
+    while(true){
+        ans=ans+to_string(arr[k/fact]);
+        arr.erase(arr.begin()+k/fact);
+        if(arr.size()==0){
+            break;
+        }
+        k=k%fact;
+        fact=fact/arr.size();
+    }
+    return ans;
+}
+
+/*Leetcode solution*/
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<int>arr;
+        string ans="";
+        int fact=1;
+
+        for(int i=1;i<n;i++){
+            fact=fact*i;
+            arr.push_back(i);
+        }
+        arr.push_back(n);
+        k=k-1;
+
+        while(true){
+            ans=ans+to_string(arr[k/fact]);
+            arr.erase(arr.begin()+k/fact);
+            if(arr.size()==0){
+                break;
+            }
+            k=k%fact;
+            fact=fact/arr.size();
+        }
+        return ans;  
+    }
+};
