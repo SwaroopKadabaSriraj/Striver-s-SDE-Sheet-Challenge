4a11060ceea5aa40f74cf0f57e076f3be5994f40
SwaroopKS_Appi0007 <106536825+SwaroopKadabaSriraj@users.noreply.github.com>
2023-10-17 23:44:54 +0530

    Update 55 - Print all permutations of a string or array.cpp

--- Day - 10/55 - Print all permutations of a string or array.cpp
+++ Day - 10/55 - Print all permutations of a string or array.cpp
@@ -22,3 +22,47 @@ vector<string> findPermutations(string &s) {
     findpermutations(index,s,ans);
     return ans;
 }
+
+/*Leetcode solution*/
+
+class Solution {
+public:
+	void permuteRecursive(vector<int> &num, int begin, vector<vector<int> > &result)	{
+		if (begin >= num.size()) {
+		    result.push_back(num);
+		    return;
+		}
+		
+		for (int i = begin; i < num.size(); i++) {
+		    swap(num[begin], num[i]);
+		    permuteRecursive(num, begin + 1, result);
+		    swap(num[begin], num[i]);
+		}
+    }
+    
+    vector<vector<int>> permute(vector<int>& num) {
+        vector<vector<int>> result;
+	    permuteRecursive(num, 0, result);
+	    return result;
+    }
+};class Solution {
+public:
+	void permuteRecursive(vector<int> &num, int begin, vector<vector<int> > &result)	{
+		if (begin >= num.size()) {
+		    result.push_back(num);
+		    return;
+		}
+		
+		for (int i = begin; i < num.size(); i++) {
+		    swap(num[begin], num[i]);
+		    permuteRecursive(num, begin + 1, result);
+		    swap(num[begin], num[i]);
+		}
+    }
+    
+    vector<vector<int>> permute(vector<int>& num) {
+        vector<vector<int>> result;
+	    permuteRecursive(num, 0, result);
+	    return result;
+    }
+};
