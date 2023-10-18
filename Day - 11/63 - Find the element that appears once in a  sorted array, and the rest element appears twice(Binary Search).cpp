8a28072f45af4920873f3fd6e127f10b8d7acf9e
SwaroopKS_Appi0007 <106536825+SwaroopKadabaSriraj@users.noreply.github.com>
2023-10-18 23:50:10 +0530

    Update 63 - Find the element that appears once in a  sorted array, and the rest element appears twice(Binary Search).cpp

--- Day - 11/63 - Find the element that appears once in a  sorted array, and the rest element appears twice(Binary Search).cpp
+++ Day - 11/63 - Find the element that appears once in a  sorted array, and the rest element appears twice(Binary Search).cpp
@@ -8,3 +8,16 @@ int uniqueElement(vector<int> arr, int n)
         xr = xr^num;
     return xr;
 }
+
+/*Leetcode solution*/
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int xr{};
+        for(auto num: nums)
+            xr = xr^num;
+        return xr;
+    }
+};
