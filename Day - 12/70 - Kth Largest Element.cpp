39da799d8c398e3f8eee22d4ea68fdf0f90da1bb
SwaroopKS_Appi0007 <106536825+SwaroopKadabaSriraj@users.noreply.github.com>
2023-10-21 23:57:09 +0530

    Update 70 - Kth Largest Element.cpp

--- Day - 12/70 - Kth Largest Element.cpp
+++ Day - 12/70 - Kth Largest Element.cpp
@@ -10,3 +10,13 @@ vector<int> kthSmallLarge(vector<int> &arr, int n, int k)
     res.push_back(arr[n-k]);
     return res;
 }
+
+/*Leetcode solution*/
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        nth_element(nums.begin(), nums.begin() + k - 1, nums.end(), greater<int>());
+        return nums[k - 1];
+    }
+};
