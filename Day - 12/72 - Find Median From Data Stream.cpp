04c530531d1bd688b2bf0a364db2b05a0fd54302
SwaroopKS_Appi0007 <106536825+SwaroopKadabaSriraj@users.noreply.github.com>
2023-10-21 23:58:06 +0530

    Update 72 - Find Median From Data Stream.cpp

--- Day - 12/72 - Find Median From Data Stream.cpp
+++ Day - 12/72 - Find Median From Data Stream.cpp
@@ -31,3 +31,34 @@ void findMedian(int *arr, int n)
         cout<<obj->findMedian()<<" ";
     }
 }
+
+
+/*Leetcode solution*/
+
+class MedianFinder {
+public:
+    MedianFinder() {}
+    
+    void addNum(int num) {
+        nums.insert(num);
+        is_even = !is_even;
+        if (it == nums.end() || (is_even && (*it) <= num)) it++;
+        if (!is_even && (*it) > num) it--;
+    }
+    
+    double findMedian() {
+        return (!is_even)? *it : (*it + *prev(it)) / 2.0;
+    }
+    
+private:
+    multiset<int> nums;
+    multiset<int>::iterator it = nums.begin();
+    bool is_even = true;
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
