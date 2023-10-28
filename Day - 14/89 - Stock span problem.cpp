6e2348c2dba775fdce9f6ea3504e5218713dd02d
SwaroopKS_Appi0007 <106536825+SwaroopKadabaSriraj@users.noreply.github.com>
2023-10-28 23:30:15 +0530

    Update 89 - Stock span problem.cpp

--- Day - 14/89 - Stock span problem.cpp
+++ Day - 14/89 - Stock span problem.cpp
@@ -24,3 +24,28 @@ vector<int> findSpans(vector<int> &price) {
     }
     return ans;
 }
+
+/*Leetcode solution*/
+
+class StockSpanner {
+public:
+    StockSpanner() {
+        
+    }
+    stack<pair<int, int>> s;
+    int next(int price) {
+        int res = 1;
+        while (!s.empty() && s.top().first <= price) {
+            res += s.top().second;
+            s.pop();
+        }
+        s.push({price, res});
+        return res;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
