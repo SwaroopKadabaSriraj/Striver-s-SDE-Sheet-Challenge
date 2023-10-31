261933c4df53b9af066e35390aec129c9fe2f5fa
SwaroopKS_Appi0007 <106536825+SwaroopKadabaSriraj@users.noreply.github.com>
2023-10-31 23:44:19 +0530

    Update 97 - Rabin Karp.cpp

--- Day - 15/97 - Rabin Karp.cpp
+++ Day - 15/97 - Rabin Karp.cpp
@@ -42,5 +42,18 @@ vector<int> stringMatch(string &str, string &pat) {
             }
         }
     }
+
+/*Leetcode solution*/
+
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
+        string as = a;
+        for (int rep = 1; rep <= b.length() / a.length() + 2; rep++, as += a)
+            if (as.find(b) != string::npos) 
+                return rep;
+        return -1;
+    }
+};
     return ans;
 }
