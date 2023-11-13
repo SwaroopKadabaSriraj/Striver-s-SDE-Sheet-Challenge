f557384c385164aec7037ea874db4fae12c8f346
SwaroopKS_Appi0007 <106536825+SwaroopKadabaSriraj@users.noreply.github.com>
2023-11-13 23:52:39 +0530

    Update 117 - Height of a Binary Tree.cpp

--- Day - 18/117 - Height of a Binary Tree.cpp
+++ Day - 18/117 - Height of a Binary Tree.cpp
@@ -61,3 +61,25 @@ int heightOfTheTree(vector<int>& inorder, vector<int>& level, int N)
     }
    return mxheight;
 }
+/*Leetcode solution*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root) return 0;
+        int maxLeft = maxDepth(root->left);
+        int maxRight = maxDepth(root->right);
+        return max(maxLeft, maxRight)+1;
+    }
+};
