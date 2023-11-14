e8f0341c79e46946a543d5e8a937909fdb070406
SwaroopKS_Appi0007 <106536825+SwaroopKadabaSriraj@users.noreply.github.com>
2023-11-14 23:49:32 +0530

    Update 121 - Check if two trees are identical or not.cpp

--- Day - 18/121 - Check if two trees are identical or not.cpp
+++ Day - 18/121 - Check if two trees are identical or not.cpp
@@ -27,3 +27,25 @@ bool identicalTrees(BinaryTreeNode<int>* root1, BinaryTreeNode<int>* root2) {
     if(root1 == NULL || root2 == NULL) return false;
     return ((root1->data == root2->data) && identicalTrees(root1->left, root2->left) && identicalTrees(root1->right, root2->right));
 }
+
+/*Leetcode solution*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL || q == NULL) 
+            return (p == q);
+        return (p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+    }
+};
