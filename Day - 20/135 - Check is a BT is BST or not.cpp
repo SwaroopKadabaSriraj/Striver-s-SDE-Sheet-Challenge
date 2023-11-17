3cab6000cd1c1b8407f2165fc4afb390e4aa9fed
SwaroopKS_Appi0007 <106536825+SwaroopKadabaSriraj@users.noreply.github.com>
2023-11-17 22:22:21 +0530

    Update 135 - Check is a BT is BST or not.cpp

--- Day - 20/135 - Check is a BT is BST or not.cpp
+++ Day - 20/135 - Check is a BT is BST or not.cpp
@@ -34,3 +34,30 @@ bool validateBST(BinaryTreeNode<int> *root) {
     // Write your code here
     return checkBST(root,INT_MIN,INT_MAX);  
 }
+
+/*Leetcode solution*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return isValidBST(root, NULL, NULL);
+    }
+
+    bool isValidBST(TreeNode* root, TreeNode* minNode, TreeNode* maxNode) {
+        if(!root) return true;
+        if(minNode && root->val <= minNode->val || maxNode && root->val >= maxNode->val)
+            return false;
+        return isValidBST(root->left, minNode, root) && isValidBST(root->right, root, maxNode);
+    }
+};
