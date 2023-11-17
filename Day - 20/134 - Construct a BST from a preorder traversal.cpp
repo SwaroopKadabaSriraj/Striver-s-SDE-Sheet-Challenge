d495b6c6dc01aff579b780fdf9a9382e0bc170cd
SwaroopKS_Appi0007 <106536825+SwaroopKadabaSriraj@users.noreply.github.com>
2023-11-17 22:21:33 +0530

    Update 134 - Construct a BST from a preorder traversal.cpp

--- Day - 20/134 - Construct a BST from a preorder traversal.cpp
+++ Day - 20/134 - Construct a BST from a preorder traversal.cpp
@@ -46,3 +46,28 @@ TreeNode<int>* preOrderTree(vector<int> &preorder){
     sort(preorder.begin(), preorder.end());
     return bstFromInorder(0, preorder.size()-1, preorder);
 }
+
+/*Leetcode solution*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int i = 0;
+    TreeNode* bstFromPreorder(vector<int>& A, int bound = INT_MAX) {
+        if (i == A.size() || A[i] > bound) return NULL;
+        TreeNode* root = new TreeNode(A[i++]);
+        root->left = bstFromPreorder(A, root->val);
+        root->right = bstFromPreorder(A, bound);
+        return root;
+    }
+};
