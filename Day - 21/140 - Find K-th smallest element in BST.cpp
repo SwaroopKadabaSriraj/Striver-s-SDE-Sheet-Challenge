84280a34db53abdca38de2bf4615cae27e641702
SwaroopKS_Appi0007 <106536825+SwaroopKadabaSriraj@users.noreply.github.com>
2023-11-21 23:22:20 +0530

    Update 140 - Find K-th smallest element in BST.cpp

--- Day - 21/140 - Find K-th smallest element in BST.cpp
+++ Day - 21/140 - Find K-th smallest element in BST.cpp
@@ -35,3 +35,36 @@ int kthSmallest(TreeNode<int> *root, int k)
     inorder(root, k);
     return ans;
 }
+
+/*Leetcode solution*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans;
+    
+    int kthSmallest(TreeNode* root, int k) {
+        inorder(root, k);
+        return ans;
+    }
+    
+    void inorder(TreeNode* root, int& k) {
+        if (!root) return;
+        inorder(root->left, k);
+        if (--k == 0){
+            ans = root->val;
+            return;
+        } 
+        inorder(root->right, k);
+    } 
+};
