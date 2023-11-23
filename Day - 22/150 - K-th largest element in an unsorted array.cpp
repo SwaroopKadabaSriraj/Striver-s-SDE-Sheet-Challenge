f7a29e189557f4c6104110c1dcf901d7f991d888
SwaroopKS_Appi0007 <106536825+SwaroopKadabaSriraj@users.noreply.github.com>
2023-11-23 23:49:31 +0530

    Update 150 - K-th largest element in an unsorted array.cpp

--- Day - 22/150 - K-th largest element in an unsorted array.cpp
+++ Day - 22/150 - K-th largest element in an unsorted array.cpp
@@ -7,3 +7,13 @@ int kthLargest(vector<int>& arr, int size, int K)
     sort(arr.begin(),arr.end());
     return arr[size-K];
 }
+
+/*Leetcode solution*/
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size()-k];
+    }
+};
