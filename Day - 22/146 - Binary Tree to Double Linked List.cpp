9c1dee45f150734dbc11450f8ab69f7138250c4c
SwaroopKS_Appi0007 <106536825+SwaroopKadabaSriraj@users.noreply.github.com>
2023-11-23 23:47:44 +0530

    Update 146 - Binary Tree to Double Linked List.cpp

--- Day - 22/146 - Binary Tree to Double Linked List.cpp
+++ Day - 22/146 - Binary Tree to Double Linked List.cpp
@@ -42,3 +42,31 @@ BinaryTreeNode<int>* BTtoDLL(BinaryTreeNode<int>* root) {
     helper(root, head, prev);
     return head;
 }
+
+/*Leetcode solution*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        TreeNode* curr = root;
+        while (curr) {
+            if (curr->left) {
+                TreeNode* runner = curr->left;
+                while (runner->right != nullptr) runner = runner->right;
+                runner->right = curr->right, curr->right = curr->left, curr->left = nullptr;
+            }
+            curr = curr->right;
+        }
+    }
+};
